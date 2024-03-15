a95e51a37eb9b9ba9d4bbf04e77a3b1282a050fd
leonardo.lobo <leonardogomeslobo@gmail.com>
2024-03-14 21:10:55 -0300

    add mainApp and logic router

--- app/src/main/java/br/com/example/spotify/MainActivity.kt
+++ app/src/main/java/br/com/example/spotify/MainActivity.kt
@@ -4,17 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
-class MainActivity() : ComponentActivity() {
+class MainActivity : ComponentActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
 
+        setContent {
+            MainApp()
         }
     }
-
-
 
 }
 
---
+++ app/src/main/java/br/com/example/spotify/MainApp.kt
@@ -0,0 +1,29 @@
+package br.com.example.spotify
+
+import Routes
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import br.com.example.spotify.ui.view.ListSongsScreen
+import br.com.example.spotify.ui.view.PlaySongScreen
+
+@Composable
+fun MainApp() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routes.ListSongs.name) {
+        composable(Routes.ListSongs.name) {
+                ListSongsScreen(navController)
+        }
+        composable(Routes.PlaySong.name + "/{title}", arguments = listOf(navArgument("title") {
+            type = NavType.StringType
+        })) {
+            PlaySongScreen(navController, it.arguments?.getString("title"))
+        }
+    }
+
+
+}
