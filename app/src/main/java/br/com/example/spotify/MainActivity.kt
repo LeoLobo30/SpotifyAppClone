830ebe5c8162eddd5235668ce21eea800cc9df00
leonardo.lobo <leonardogomeslobo@gmail.com>
2024-03-22 00:19:48 -0300

    config Koin in MainActivity for use in another classes

--- app/src/main/java/br/com/example/spotify/MainActivity.kt
+++ app/src/main/java/br/com/example/spotify/MainActivity.kt
@@ -3,20 +3,49 @@ package br.com.example.spotify
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.room.Room
+import br.com.example.spotify.data.firebase.impl.RepositoryFirebaseFirestoreImpl
+import br.com.example.spotify.data.room.database.SongDatabase
+import br.com.example.spotify.ui.viewModel.ListSongsViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.compose.KoinApplication
+import org.koin.core.module.Module
+import org.koin.dsl.module
 
 class MainActivity : ComponentActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            MainApp()
+            KoinApplication(application = {
+                androidContext(this@MainActivity)
+                androidLogger()
+                modules(initKoin())
+            }) {
+                MainApp()
+            }
         }
+
     }
 
+    private fun initKoin(): List<Module> {
+        val roomModule = module {
+            single {
+                Room.databaseBuilder(androidContext(), SongDatabase::class.java, "song_database")
+                    .build().songDAO()
+            }
+        }
+
+        val viewModel = module {
+            viewModel { ListSongsViewModel(get(), get()) }
+        }
+
+        val firebaseModule = module {
+            single { RepositoryFirebaseFirestoreImpl(false) }
+        }
+
+        return listOf(roomModule, viewModel, firebaseModule)
+    }
 }
-
-
-
-
