488277a6a87b7d3b826d770718821d4575ae4570
leonardo.lobo <leonardogomeslobo@gmail.com>
2025-06-03 00:31:44 -0300

    fazendo ajustes em arquivos de configuracao

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     kotlin("kapt") version "1.9.0"
+    id("com.android.library") version "8.6.1" apply false
 }
 
 buildscript {
